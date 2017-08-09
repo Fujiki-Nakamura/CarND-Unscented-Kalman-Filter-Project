c018649a3b8b27a9e500df281f86991ca9d33bc1
fujiki <abcdef.nakamura@gmail.com>
2017-08-09 10:30:27 +0900

    modify

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -20,9 +20,9 @@ UKF::UKF() {
   // initial covariance matrix
   P_ = MatrixXd(5, 5);
   // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 1.5;
+  std_a_ = 0.1;
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 0.55;
+  std_yawdd_ = 0.5;
   // Laser measurement noise standard deviation position1 in m
   std_laspx_ = 0.15;
   // Laser measurement noise standard deviation position2 in m
@@ -64,49 +64,41 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
   Complete this function! Make sure you switch between lidar and radar
   measurements.
   */
-  if ((meas_package.sensor_type_ == MeasurementPackage::RADAR) ||
-      (meas_package.sensor_type_ == MeasurementPackage::LASER)) {
-    if (!is_initialized_) {
-      x_ << 0, 0, 0, 0, 0;
-      P_ << 1, 0, 0, 0, 0,
-            0, 1, 0, 0, 0,
-            0, 0, 1, 0, 0,
-            0, 0, 0, 1, 0,
-            0, 0, 0, 0, 1;
-      time_us_ = meas_package.timestamp_;
-      
-      if (meas_package.sensor_type_ == MeasurementPackage::LASER && use_laser_) {
-        x_(0) = meas_package.raw_measurements_(0);
-        x_(1) = meas_package.raw_measurements_(1);
-      }
-      else if (meas_package.sensor_type_ == MeasurementPackage::RADAR && use_radar_) {
-        float rho = meas_package.raw_measurements_(0);
-        float phi = meas_package.raw_measurements_(1);
-        float rho_dot = meas_package.raw_measurements_(2);
-        float vx = rho_dot * cos(phi);
-        float vy = rho_dot * sin(phi);
-        float v  = sqrt(vx * vx + vy * vy);
-        x_(0) = rho * cos(phi);
-        x_(1) = rho * sin(phi);
-        x_(2) = sqrt(vx * vx + vy * vy);
-      }
-      is_initialized_ = true;
-
-      return;
-    }
-
-    // Prediction
-    float dt = (meas_package.timestamp_ - time_us_) / 1000000.0;
+  if (!is_initialized_) {
+    x_ << 1, 1, 0.2, 0, 0.1;
+    P_ << 1, 0, 0, 0, 0,
+          0, 1, 0, 0, 0,
+          0, 0, 1, 0, 0,
+          0, 0, 0, 1, 0,
+          0, 0, 0, 0, 1;
     time_us_ = meas_package.timestamp_;
-    Prediction(dt);
+    
+    if (meas_package.sensor_type_ == MeasurementPackage::LASER && use_laser_) {
+      x_(0) = meas_package.raw_measurements_(0);
+      x_(1) = meas_package.raw_measurements_(1);
+    }
+    else if (meas_package.sensor_type_ == MeasurementPackage::RADAR && use_radar_) {
+      float rho = meas_package.raw_measurements_(0);
+      float phi = meas_package.raw_measurements_(1);
+      x_(0) = rho * cos(phi);
+      x_(1) = rho * sin(phi);
+    }
+    is_initialized_ = true;
 
-    // Update
-    if (meas_package.sensor_type_ == MeasurementPackage::LASER) {
-      UpdateLidar(meas_package);
-    }
-    if (meas_package.sensor_type_ == MeasurementPackage::RADAR) {
-      UpdateRadar(meas_package);
-    }
+    return;
+  }
+
+  // Prediction
+  float dt = (meas_package.timestamp_ - time_us_) / 1000000.0;
+  time_us_ = meas_package.timestamp_;
+  Prediction(dt);
+
+  // Update
+  if (meas_package.sensor_type_ == MeasurementPackage::LASER) {
+    UpdateLidar(meas_package);
+  }
+  if (meas_package.sensor_type_ == MeasurementPackage::RADAR) {
+    UpdateRadar(meas_package);
   }
 }
 
@@ -138,6 +130,7 @@ void UKF::Prediction(double delta_t) {
   x_aug.head(5) = x_;
   x_aug(5) = 0.0;
   x_aug(6) = 0.0;
+
   P_aug.fill(0.0);
   P_aug.topLeftCorner(5, 5) = P_;
   P_aug(5, 5) = std_a_ * std_a_;
@@ -184,6 +177,7 @@ void UKF::Prediction(double delta_t) {
     Xsig_pred_(3, i) = yaw_pred;
     Xsig_pred_(4, i) = yawd_pred;
   }
+
   double weight_0 = lambda_ / (lambda_ + n_aug_);
   weights_(0) = weight_0;
   for (int i = 1; i < 2 * n_aug_ + 1; i++) {
